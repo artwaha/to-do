6cf1902963d007b00036bf3fa1058889440b0961
Abdul-razak Twaha <abdul-razak.twaha@outlook.com>
2023-02-15 23:28:53 +0300

    Navbar Modifications 1

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -10,13 +10,13 @@ export default function Navbar() {
     ];
     return (
         <div className="w-full">
-            <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between">
+            <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between border-b border-gray-200">
                 {/* Logo  */}
                 <Disclosure>
                     {({ open }) => (
                         <>
                             <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
-                                <Link to="#" className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
+                                <Link to="#" className="flex font-extrabold items-center space-x-2 text-2xl text-indigo-500 dark:text-gray-100">
                                     To-do
                                 </Link>
 
